17565403930dea69715b435cc8bbc812e4cfaf18
Devika18 <devikankarpe@gmail.com>
2022-10-29 21:28:36 +0530

    added: logic for adding nodes in BST

--- src/MyBinaryTreeMain.java
+++ src/MyBinaryTreeMain.java
@@ -1,5 +1,12 @@
 public class MyBinaryTreeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Binary Search Tree Program");
+
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+
+        myBinaryTree.printBinaryTree();
     }
 }
